8486935c6680141e6699d2d58443c2c7f666c775
Phil Underwood <beardydoc@gmail.com>
2018-04-12 23:38:40 +0100

    Add shininess
    Signed-off-by: Phil Underwood <beardydoc@gmail.com>

---
+++ spinny.py
@@ -0,0 +1,39 @@
+import cubertmpl as cubert
+import time
+import itertools
+
+DELAY = 0.1
+
+start_time = time.time()
+
+COLOURS = (
+    (255, 0, 0),
+    (255, 255, 0),
+    (0, 255, 0),
+    (0, 255, 255),
+    (0, 0, 255),
+    (255, 0, 255),
+    (255, 0, 0))
+
+def colour_func(x, y, z):
+    t = time.time()-start_time
+    pos = (x+y+z+t) % 21
+    index = int(pos*6.0/21)
+    offset = (pos*6.0/21)- index
+    c1 = COLOURS[index]
+    c2 = COLOURS[index+1]
+    colour = [c1[i]*(1.0-offset) + c2[i]*offset for i in range(3)]
+    return colour
+
+cube = cubert.Cubert()
+while True:
+    cube.clear()
+    cube.set_pixel(0,0,0, (255,0,0)) # FBL RED
+    cube.set_pixel(7,0,0, (0, 255, 0)) # FBR GREEN
+    cube.set_pixel(0,7,0, (0,0, 255)) #FTL BLUE
+    cube.set_pixel(0,0,7, (0, 255, 255)) #BBL CYAN
+    #for i, j, k in itertools.product(range(8), repeat=3):
+    #    cube.set_pixel(i, j, k, colour_func(i, j, k))
+    cube.show()
+    cubert.plt.show()
+    time.sleep(DELAY)
